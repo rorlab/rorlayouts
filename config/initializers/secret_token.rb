6fcc1325350b8016cca3462632215b7c51ce41d8
Hyoseong Choi <hschoidr@gmail.com>
2012-12-05 21:15:01 +0900

    최초커밋

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorlayouts::Application.config.secret_token = 'f5ba111e193538b98e121c5bf98ba68543ef93e2c4b49787a291dca8e844a4c2faaed863c32f774acd128db39f81cd963c5d903b4f81538c4dade2d40477090f'
